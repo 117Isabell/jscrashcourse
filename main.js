59836ab5993d8e87c38e69e3909b4abc3ee07da9
117Isabell <isabellhepburn@outlook.com>
2022-11-21 16:44:16 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// console.log('Hello World');
+// console.error('This is an error');
+// console.warn('This is a warning');
+
+
+// (var), let(reassign values), const
+
+
+// data types: strings, numbers, booleans, null, undefined 
+//const name = 'Isabell';
+//const age = 30;
+//const rating = 4.5;
+//const isCool = true;
+//const  x = null;
+//const y = undefined;
+//let z;
+//console.log(typeof y);
+
+// Contatenation 
+const name = 'Isabell';
+const age = 30;
+// console.log('My name is ' + name + ' and I am ' + age);
+
+
+// Template String(Better)
+console.log(`My name is ${name} and I am ${age}`);
+
+
+
+
